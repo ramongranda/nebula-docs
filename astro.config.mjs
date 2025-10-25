5c7942aa6ce02bfe7e7357f6b6c6860b2f288b9e
Ramón Granda García <ramongranda@gmail.com>
2025-10-25 18:18:39 +0200

    feature: autoDraft

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,53 +2,36 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import svelte from "@astrojs/svelte";
-import { buildSidebarFromFS } from "./src/config/sidebar-fs.mjs";
-import starlightAutoDrafts from "starlight-auto-drafts";
 import mermaid from "astro-mermaid";
+import starlightAutoDrafts from "starlight-auto-drafts";
+import { buildSidebarFromFS } from "./src/config/sidebar-fs.mjs";
 
 const isDev = process.env.NODE_ENV === "development";
 const showReference = process.env.SHOW_REFERENCE === "true";
 
-// https://astro.build/config
+const baseSidebar = buildSidebarFromFS({ isDev, showReference });
+const sidebar = [...baseSidebar];
+
 export default defineConfig({
   integrations: [
     starlight({
       title: "Nebula Docs",
-      customCss: [
-        // Relative path to your custom CSS file
-        "./src/styles/fonts.css",
-        "./src/styles/custom.css",
-      ],
+      customCss: ["./src/styles/fonts.css", "./src/styles/custom.css"],
       logo: {
         light: "./src/assets/light-logo.svg",
         dark: "./src/assets/dark-logo.svg",
         replacesTitle: true,
       },
-      components: {
-        // Override component.
-      },
       social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/ramongranda/nebula-docs",
-        },
+        { icon: "github", label: "GitHub", href: "https://github.com/ramongranda/nebula-docs" },
       ],
-      sidebar: buildSidebarFromFS({ isDev, showReference }),
+      sidebar,
     }),
-    starlightAutoDrafts({
-      highlights: {
-        badges: false,
-      },
-    }),
+    starlightAutoDrafts({ highlights: { badges: false } }),
     svelte(),
     mermaid({
       autoTheme: true,
-      mermaidConfig: {
-        flowchart: {
-          curve: "basis",
-        },
-      },
+      mermaidConfig: { flowchart: { curve: "basis" } },
     }),
   ],
 });
